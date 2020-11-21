4f89041921efae0d1e63710791b13354d4cb0932
ijbd <ijbd@umich.edu>
2020-11-21 14:48:04 -0700

    Figure 2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Wide-Area Renewables Planning (WARP) Paper Results
 
- ### **Reliability Benefits of Wide-Area Renewable Energy Planning:  EffectiveLoad Carrying Capabilities of Wind and Solar Power across the Western United States** (2020)
+ ### **Reliability Benefits of Wide-Area Renewable Energy Planning:  Effective Load Carrying Capabilities of Wind and Solar Power across the Western United States** (2020)
 
  **Authors:** I. Bromley-Dulfano, J. Florez, M.Craig
 
--- src/filter_offshore.py
+++ src/filter_offshore.py
@@ -2,23 +2,28 @@ import pandas as pd
 import numpy as np
 
 origin_results_folder = '../map_results'
-destination_results_folder = '../map_results_onshore'
+destination_results_folder = '../map_results_onshore_2019'
 
 onshore_bounds = pd.read_excel("offshore_bounds/offshore_MERRA_Format_bounds.xlsx",index_col=0).values
 
-def filter_offshore(region,year,capacity,technology,add_on=None):
+def filter_offshore(region,year,capacity,technology,add_on=None,scale_down=False):
     elcc_map_file = '%s/%s_%d_%d_MW_%s%s_results_map.csv' % (origin_results_folder,region,year,capacity,technology,'' if add_on is None else '_'+add_on)
     elcc_df = pd.read_csv(elcc_map_file,index_col=0,header=0)
     elcc = elcc_df.to_numpy()
     lats = np.asarray(elcc_df.index.values)
     lons = np.asarray(elcc_df.columns.values).astype(float)
 
+    if scale_down:
+        elcc = elcc[1::2,1::2]
+        lats = lats[1::2]
+        lons = lons[1::2]
+
     #if cell is onshore (0) leave value as is, if offshore (1) place value as np.nan
     onshore_map = np.where(onshore_bounds == 0, elcc, np.nan)
     
     elcc = onshore_map
 
-    results_filename = '%s_%d_%d_MW_%s%s_results.csv' % (region,year,capacity,technology,'' if add_on is None else '_'+add_on)
+    results_filename = '%s_%d_%d_MW_%s%s_results_map.csv' % (region,year,capacity,technology,'' if add_on is None else '_'+add_on)
     
     elcc_map = pd.DataFrame(index=lats, columns=lons, data=elcc)
     elcc_map.to_csv('%s/%s' % (destination_results_folder, results_filename))
@@ -27,13 +32,11 @@ def filter_offshore(region,year,capacity,technology,add_on=None):
 # Base cases
 capacity = 500
 for region in ['basin','california','mountains','northwest','southwest']:
-    for year in [2016, 2017, 2018, 2019]:
+    for year in [2019]:
         for technology in ['solar', 'wind']:
-            for ff in [True, False]:
-                try: 
-                    filter_offshore(region,year,capacity,technology,'FF' if ff else '')
-                except:
-                    pass
+            filter_offshore(region,year,capacity,technology,scale_down=True)
+            filter_offshore(region,year,capacity,technology,'FF',scale_down=True)
+                
 
 # Sensitivities
 year = 2019
@@ -42,13 +45,8 @@ for region in ['basin','california','mountains','northwest','southwest']:
         for capacity in [100,500,2000]:
             # storage
             if capacity == 500:
-                try: 
-                    filter_offshore(region,year,capacity,technology,'storage')
-                except:
-                    pass
+                filter_offshore(region,year,capacity,technology,'storage')
             # variable size
             else: 
-                try: 
-                    filter_offshore(region,year,capacity,technology)
-                except:
-                    pass
+                filter_offshore(region,year,capacity,technology)
+                
