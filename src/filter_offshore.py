2f35a45380788f7cca402b6978005f73349cb5da
ijbd <ijbd@umich.edu>
2020-11-22 12:15:09 -0700

    Onshore results.

--- src/filter_offshore.py
+++ src/filter_offshore.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 
 origin_results_folder = '../map_results'
-destination_results_folder = '../map_results_onshore_2019'
+destination_results_folder = '../map_results_onshore'
 
-onshore_bounds = pd.read_excel("offshore_bounds/offshore_MERRA_Format_bounds.xlsx",index_col=0).values
+all_onshore_bounds = pd.read_excel("offshore_bounds/offshore_MERRA_Format_bounds.xlsx",index_col=0).values
 
 def filter_offshore(region,year,capacity,technology,add_on=None,scale_down=False):
     elcc_map_file = '%s/%s_%d_%d_MW_%s%s_results_map.csv' % (origin_results_folder,region,year,capacity,technology,'' if add_on is None else '_'+add_on)
@@ -13,10 +13,8 @@ def filter_offshore(region,year,capacity,technology,add_on=None,scale_down=False
     lats = np.asarray(elcc_df.index.values)
     lons = np.asarray(elcc_df.columns.values).astype(float)
 
-    if scale_down:
-        elcc = elcc[1::2,1::2]
-        lats = lats[1::2]
-        lons = lons[1::2]
+    onshore_bounds = all_onshore_bounds[1::2,1::2] if scale_down else all_onshore_bounds
+
 
     #if cell is onshore (0) leave value as is, if offshore (1) place value as np.nan
     onshore_map = np.where(onshore_bounds == 0, elcc, np.nan)
@@ -32,10 +30,10 @@ def filter_offshore(region,year,capacity,technology,add_on=None,scale_down=False
 # Base cases
 capacity = 500
 for region in ['basin','california','mountains','northwest','southwest']:
-    for year in [2019]:
+    for year in [2016, 2017, 2018, 2019]:
         for technology in ['solar', 'wind']:
-            filter_offshore(region,year,capacity,technology,scale_down=True)
-            filter_offshore(region,year,capacity,technology,'FF',scale_down=True)
+            filter_offshore(region,year,capacity,technology)
+            filter_offshore(region,year,capacity,technology,'FF')
                 
 
 # Sensitivities
@@ -45,8 +43,8 @@ for region in ['basin','california','mountains','northwest','southwest']:
         for capacity in [100,500,2000]:
             # storage
             if capacity == 500:
-                filter_offshore(region,year,capacity,technology,'storage')
+                filter_offshore(region,year,capacity,technology,'storage',scale_down=True)
             # variable size
             else: 
-                filter_offshore(region,year,capacity,technology)
+                filter_offshore(region,year,capacity,technology,scale_down=True)
                 
